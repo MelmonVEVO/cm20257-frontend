13904a9021b79d1413588d1b71dff978b84a7ca8
Melmon" was taken <dylan.drescher@gmail.com>
2021-02-04 16:43:49 +0000

    Skeleton uploaded

---
+++ app/src/main/java/com/cm20257/frontend/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cm20257.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
